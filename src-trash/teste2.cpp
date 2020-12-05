49c12defc4b756ca5c2f6319c9c94da46ac3f509
Lucas Custódio <lucascustodio@hotmail.com.br>
2020-12-04 22:49:13 -0300

    Update

---
+++ src-trash/teste2.cpp
@@ -0,0 +1,27 @@
+/*
+	Initialise Winsock
+*/
+
+#include<stdio.h>
+#include<winsock2.h>
+#include <cstdio>
+
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+int main(int argc , char *argv[])
+{
+	WSADATA wsa;
+	
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	{
+		printf("Failed. Error Code : %d",WSAGetLastError());
+		return 1;
+	}
+	
+	printf("Initialised.");
+	
+	
+	std::getchar();
+	return 0;
+}
